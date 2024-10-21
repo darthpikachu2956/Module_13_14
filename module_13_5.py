82a6b54188443d61f16335599b54815e0cc02443
Vlad <darthpikachu2956@gmail.com>
2024-10-22 01:56:32 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,73 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = "xxx"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text="Count")
+button2 = KeyboardButton(text='Information')
+keyboard.add(button)
+keyboard.add(button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Count")
+async def set_age(message):
+    await message.answer("Input your age, please.")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Input your growth, please.")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Input your weight, please.")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = float(data.get('growth'))
+    weight = float(data.get('weight'))
+    result = (10 * weight) + (6.25 * growth) - (5 * age) + 5
+    await message.answer(f"Your normal calories consuming is {result}.")
+    await state.finish()
+
+
+@dp.message_handler(commands=["start"])
+async def start(message):
+    await message.answer("Hello! I am a healthcare bot!", reply_markup=keyboard)
+
+
+@dp.message_handler(text="Information")
+async def inform(message):
+    await message.answer("Information about this bot.")
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Please input the '/start' command in order to begin the communication.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
